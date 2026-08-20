f25c6e911347d111968147abb764ccb907c2c9eb
Tyler McClanahan <tmak1787@gmail.com>
2026-08-19 21:59:15 -0600

    try putting a line between window title tabs in bar

--- dwm.c
+++ dwm.c
@@ -833,6 +833,8 @@ drawbar(Monitor *m)
 					remainder--;
 				}
 				drw_text(drw, x, 0, tabw, bh, lrpad / 2, c->name, 0);
+				drw_setscheme(drw, scheme[SchemeNorm]);
+				drw_rect(drw, x + tabw - 1, 3, 1, bh - 6, 1, 0);
 				x += tabw;
 			}
 		} else {
